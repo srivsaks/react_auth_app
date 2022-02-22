ca691293d179d16a88e97ad6580ae21622721104
Sakshi Srivastava <srivsaks@amazon.com>
2022-02-22 10:13:50 +0530

    i dont know what i did in this commit...but app is now completed

--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -1,4 +1,4 @@
-import React,{useState} from "react";
+import React,{useState,useEffect,useCallback} from "react";
 
 
 let autoLogOutTimer;
@@ -8,6 +8,23 @@ const AuthContext=React.createContext({
     login:(token)=>{},
     logout:()=>{}
 })
+
+const retrieveStoredToken=()=>{
+  const storedToken=localStorage.getItem("token");
+  const storedExpirationDate=localStorage.getItem("expirationTime");
+const remainingTime=calculateRemainingTime(storedExpirationDate);
+
+if(remainingTime<=3600){
+    localStorage.removeItem("token");
+    localStorage.removeItem("expirationTime");
+    return null;
+}
+
+return {
+    token:storedToken,
+    duration:remainingTime
+}
+}
 
 const calculateRemainingTime=(expirationTime)=>{
     // get current time;
@@ -21,20 +38,31 @@ const calculateRemainingTime=(expirationTime)=>{
 }
 
 export const AuthConextProvider=(props)=>{
-const storedToken=localStorage.getItem("token");
-const[token,setToken]=useState(storedToken);
+    const tokenData=retrieveStoredToken();
+    let initialToken;
+    if(tokenData){
+        initialToken=tokenData.token
+    }
+const[token,setToken]=useState(initialToken);
 const isUserLoggedIn=!!token;
 
-const logoutHandler=()=>{
+useEffect(()=>{
+if(tokenData){
+    autoLogOutTimer=setTimeout(logoutHandler,tokenData.duration)
+}
+},[tokenData,logoutHandler]);
+
+const logoutHandler=useCallback(()=>{
     setToken(null)
     localStorage.clear();
     if(autoLogOutTimer){
     clearTimeout(autoLogOutTimer);}
-}
+},[])
 
 const loginHandler=(token,expirationTime)=>{
     setToken(token)
     localStorage.setItem("token",token);
+    localStorage.setItem("expirationTime",expirationTime);
     const remainingTime=calculateRemainingTime(expirationTime);
     autoLogOutTimer= setTimeout(logoutHandler,remainingTime)
 }
